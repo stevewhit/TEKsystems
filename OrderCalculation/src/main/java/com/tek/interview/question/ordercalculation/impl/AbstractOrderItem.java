1d6b60b7b99ccd3235719b80b0055c621799e539
Steve Whitmire <swhit114@gmail.com>
2016-12-22 16:18:28 -0500

    Created AbstractOrderItem and it's JUnit4 tests.

---
+++ OrderCalculation/src/main/java/com/tek/interview/question/ordercalculation/impl/AbstractOrderItem.java
@@ -0,0 +1,127 @@
+
+package com.tek.interview.question.ordercalculation.impl;
+
+import java.text.DecimalFormat;
+
+import com.tek.interview.question.ordercalculation.OrderItem;
+
+/**
+ * Creates an abstract order item containing a name, unit price, and quantity
+ * @author Steve Whitmire (swhit114@gmail.com)
+ *
+ */
+public abstract class AbstractOrderItem implements OrderItem
+{
+	/**
+	 * The name/description of the order item.
+	 */
+	private String name;
+	
+	/**
+	 * Price for one unit of the order item.
+	 */
+	private double unitPrice;
+	
+	/**
+	 * The amount of these items to be added to the order
+	 */
+	private int quantity;
+	
+	/**
+	 * Constructor that accepts a name and unit price. By default the quantity is set to 1.
+	 * @param name - The name or description of the order item. 
+	 * @param unitPrice - Price for one unit of the order item.
+	 * @throws IllegalArgumentException - Throws if name is null or empty, or if unitPrice is < 0
+	 */
+	public AbstractOrderItem(String name, double unitPrice) throws IllegalArgumentException
+	{
+		this(name, unitPrice, 1);
+	}
+	
+	/**
+	 * Constructor that accepts a name, unit price, and a quantity
+	 * @param name - The name or description of the order item. 
+	 * @param unitPrice - Price for one unit of the order item.
+	 * @param quantity - The amount of these items to be added to order.
+	 * @throws IllegalArgumentException - Throws if name is null or empty, or if unitPrice is < 0
+	 */
+	public AbstractOrderItem(String name, double unitPrice, int quantity) throws IllegalArgumentException
+	{
+		setName(name);
+		setUnitPrice(unitPrice);
+		setQuantity(quantity);
+	}
+	
+	public String getName()
+	{
+		return this.name;
+	}
+
+	public double getUnitPrice()
+	{
+		return this.unitPrice;
+	}
+
+	public int getQuantity()
+	{
+		return this.quantity;
+	}
+	
+	/**
+	 * Returns the item in the following format "'quantity' 'name': 'unitPrice'". 
+	 * <pre>
+	 * Example: 1 box of dark chocolates: 19.85
+	 * </pre>
+	 */
+	@Override
+	public String toString()
+	{
+		DecimalFormat formatter = new DecimalFormat("0.####################################");
+		return String.format("%1$d %2$s: %3$s", this.quantity, this.name, formatter.format(this.unitPrice));
+	}
+	
+	/**
+	 * Checks for a valid name and updates the order item name if valid. 
+	 * @param name - Name or description of the order item.
+	 * @throws IllegalArgumentException - Throws if name is null or empty.
+	 */
+	private void setName(String name) throws IllegalArgumentException
+	{
+		if (name == null || name.isEmpty()) 
+		{
+			throw new IllegalArgumentException("Invalid name supplied for the item. The name must contain 1 or more characters");
+		}
+		
+		this.name = name;
+	}
+	
+	/**
+	 * Checks for a valid unit price and updates the order item unit price if valid.
+	 * @param unitPrice - Price for one unit of the order item.
+	 * @throws IllegalArgumentException - Throws if the unitPrice is less than 0.
+	 */
+	private void setUnitPrice(double unitPrice) throws IllegalArgumentException
+	{
+		if (unitPrice < 0)
+		{
+			throw new IllegalArgumentException("Invalid unitPrice for the item. The unitPrice must be >= 0.");
+		}
+		
+		this.unitPrice = unitPrice;
+	}
+	
+	/**
+	 * Checks for a valid quantity and updates the order item quantity if valid.
+	 * @param quantity - The amount of these items to be added to order.
+	 * @throws IllegalArgumentException - Throws if quantity <= 0
+	 */
+	private void setQuantity(int quantity) throws IllegalArgumentException
+	{
+		if (quantity <= 0)
+		{
+			throw new IllegalArgumentException("Invalid quantity for the item. The quantity must be > 0");
+		}
+		
+		this.quantity = quantity;
+	}
+}
---
+++ OrderCalculation/src/test/java/com/tek/interview/question/ordercalculation/impl/AbstractOrderItemTest.java
@@ -0,0 +1,176 @@
+/**
+ * 
+ */
+package com.tek.interview.question.ordercalculation.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Steve Whitmire (swhit114@gmail.com)
+ *
+ */
+public class AbstractOrderItemTest
+{
+	protected class ConcreteAbstractOrderItem extends AbstractOrderItem
+	{
+		public ConcreteAbstractOrderItem(String name, double unitPrice) throws IllegalArgumentException
+		{
+			super(name, unitPrice);
+		}
+		
+		public ConcreteAbstractOrderItem(String name, double unitPrice, int quantity) throws IllegalArgumentException
+		{
+			super(name, unitPrice, quantity);
+		}
+	}
+
+	private ConcreteAbstractOrderItem concreteAbstractOrderItem;
+	
+	/**
+	 * Initializes concreteAbstractOrderItem with name="Chocolate cake", unitPrice=123.456, and quantity=3
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception
+	{
+		concreteAbstractOrderItem = new ConcreteAbstractOrderItem("Chocolate cake", 123.456, 3);
+	}
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception
+	{
+		concreteAbstractOrderItem = null;
+	}
+	
+	// REGION: Constructor tests with Name, UnitPrice
+	/**
+	 * Test method for {@link com.tek.interview.question.ordercalculation.impl.AbstractOrderItem#AbstractOrderItem(java.lang.String, double)}.
+	 */
+	@Test(expected=IllegalArgumentException.class)
+	public void testAbstractOrderItemStringDouble_InvalidName1()
+	{
+		ConcreteAbstractOrderItem badOrderItem = new ConcreteAbstractOrderItem("", 123.33);
+	}
+	
+	/**
+	 * Test method for {@link com.tek.interview.question.ordercalculation.impl.AbstractOrderItem#AbstractOrderItem(java.lang.String, double)}.
+	 */
+	@Test(expected=IllegalArgumentException.class)
+	public void testAbstractOrderItemStringDouble_InvalidName2()
+	{
+		ConcreteAbstractOrderItem badOrderItem = new ConcreteAbstractOrderItem(null, 123.33);
+	}
+	
+	/**
+	 * Test method for {@link com.tek.interview.question.ordercalculation.impl.AbstractOrderItem#AbstractOrderItem(java.lang.String, double)}.
+	 */
+	@Test(expected=IllegalArgumentException.class)
+	public void testAbstractOrderItemStringDouble_InvalidUnitPrice()
+	{
+		ConcreteAbstractOrderItem badOrderItem = new ConcreteAbstractOrderItem("valid name", -0.00000000000000000001);
+	}
+	
+	/**
+	 * Test method for {@link com.tek.interview.question.ordercalculation.impl.AbstractOrderItem#AbstractOrderItem(java.lang.String, double)}.
+	 */
+	@Test
+	public void testAbstractOrderItemStringDouble_ValidConstructor()
+	{
+		ConcreteAbstractOrderItem goodOrderItem = new ConcreteAbstractOrderItem("valid name", 000000000000000000000000000000000.000000000000000000000000000000001);
+	}
+	
+	// REGION: Constructor tests with Name, UnitPrice, and Quantity
+	/**
+	 * Test method for {@link com.tek.interview.question.ordercalculation.impl.AbstractOrderItem#AbstractOrderItem(java.lang.String, double, int)}.
+	 */
+	@Test(expected=IllegalArgumentException.class)
+	public void testAbstractOrderItemStringDoubleInt_InvalidName1()
+	{
+		ConcreteAbstractOrderItem badOrderItem = new ConcreteAbstractOrderItem("", 123, 1);
+	}
+	
+	/**
+	 * Test method for {@link com.tek.interview.question.ordercalculation.impl.AbstractOrderItem#AbstractOrderItem(java.lang.String, double, int)}.
+	 */
+	@Test(expected=IllegalArgumentException.class)
+	public void testAbstractOrderItemStringDoubleInt_InvalidName2()
+	{
+		ConcreteAbstractOrderItem badOrderItem = new ConcreteAbstractOrderItem(null, 1234.3, 12);
+	}
+
+	/**
+	 * Test method for {@link com.tek.interview.question.ordercalculation.impl.AbstractOrderItem#AbstractOrderItem(java.lang.String, double, int)}.
+	 */
+	@Test(expected=IllegalArgumentException.class)
+	public void testAbstractOrderItemStringDoubleInt_InvalidUnitPrice()
+	{
+		ConcreteAbstractOrderItem badOrderItem = new ConcreteAbstractOrderItem("valid name", -00000000000000000000000000.0000000000000000000000001, 23);
+	}
+	
+	/**
+	 * Test method for {@link com.tek.interview.question.ordercalculation.impl.AbstractOrderItem#AbstractOrderItem(java.lang.String, double, int)}.
+	 */
+	@Test(expected=IllegalArgumentException.class)
+	public void testAbstractOrderItemStringDoubleInt_InvalidQuantity()
+	{
+		ConcreteAbstractOrderItem badOrderItem = new ConcreteAbstractOrderItem("valid name", 00000000000000000000000000.0000000000000000000000001, 0);
+	}
+	
+	/**
+	 * Test method for {@link com.tek.interview.question.ordercalculation.impl.AbstractOrderItem#AbstractOrderItem(java.lang.String, double, int)}.
+	 */
+	@Test
+	public void testAbstractOrderItemStringDoubleInt_Valid()
+	{
+		ConcreteAbstractOrderItem goodOrderItem = new ConcreteAbstractOrderItem("valid name", 00000000000000000000000000000000.000000000000000000000000000000001, 12335448);
+	}
+	
+	// END CONSTRUCTORS REGION.
+	
+	/**
+	 * Test method for {@link com.tek.interview.question.ordercalculation.impl.AbstractOrderItem#getName()}.
+	 */
+	@Test
+	public void testGetName()
+	{
+		assertEquals("Chocolate cake", concreteAbstractOrderItem.getName());
+	}
+	
+	/**
+	 * Test method for {@link com.tek.interview.question.ordercalculation.impl.AbstractOrderItem#getUnitPrice()}.
+	 */
+	@Test
+	public void testGetUnitPrice()
+	{
+		assertTrue(concreteAbstractOrderItem.getUnitPrice() == 123.456);
+	}
+	
+	/**
+	 * Test method for {@link com.tek.interview.question.ordercalculation.impl.AbstractOrderItem#getQuantity()}.
+	 */
+	@Test
+	public void testGetQuantity()
+	{
+		assertEquals(3, concreteAbstractOrderItem.getQuantity());
+	}
+	
+	/**
+	 * Test method for {@link com.tek.interview.question.ordercalculation.impl.AbstractOrderItem#toString()}.
+	 */
+	@Test
+	public void testToString()
+	{
+		assertEquals(concreteAbstractOrderItem.toString(), "3 Chocolate cake: 123.456");
+		
+		ConcreteAbstractOrderItem newOrderItem = new ConcreteAbstractOrderItem("Chocolate cake", 00000000000000000000000.000000000000000000001, 3);
+		assertEquals(newOrderItem.toString(), "3 Chocolate cake: 0.000000000000000000001");
+	}
+	
+}
