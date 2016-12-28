05b905b4468dfa4c0f3e2702b2524ce76622eca6
Steve Whitmire <swhit114@gmail.com>
2016-12-27 19:09:37 -0500

    Added TaxCalculatorUtils and JUnit4 tests. Only added basic tax calculations for 1 item so far.

---
+++ OrderCalculation/src/main/java/com/tek/interview/question/ordercalculation/impl/TaxCalculatorUtils.java
@@ -0,0 +1,66 @@
+package com.tek.interview.question.ordercalculation.impl;
+
+import java.math.BigDecimal;
+
+/**
+ * Tax utility class used for calculating tax of item/s.
+ * @author Steve Whitmire (swhit114@gmail.com)
+ *
+ */
+public final class TaxCalculatorUtils
+{
+	/**
+	 * To make it impossible to instantiate this class. 
+	 */
+	private TaxCalculatorUtils(){}
+	
+	/**
+	 * Calculates the tax amount of one item in USD. 
+	 * @param unitPrice - The price of one unit in USD.
+	 * @param taxRateDecimal - The tax rate in decimal format as a double (between 0.0 and 1.0 inclusive).
+	 * @return - Returns the tax amount of one item in USD as a double.
+	 * @throws IllegalArgumentException - Throws if unitPrice or quantity are less than 0 or if taxRateDecimal is not between 0 and 1(inclusive). 
+	 */
+	public static double calculateTaxUSD(double unitPrice, double taxRateDecimal) throws IllegalArgumentException
+	{
+		return calculateTaxUSD(unitPrice, 1, taxRateDecimal);
+	}
+	
+	/**
+	 * Calculates the tax amount of one item in USD. 
+	 * @param unitPrice - The price of one unit in USD.
+	 * @param quantity - The number of units that are to be included in the tax calculation.
+	 * @param taxRateDecimal - The tax rate in decimal format as a double (between 0.0 and 1.0 inclusive).
+	 * @return - Returns the tax amount of one item in USD as a double.
+	 * @throws IllegalArgumentException - Throws if unitPrice or quantity are less than 0 or if taxRateDecimal is not between 0 and 1(inclusive). 
+	 */
+	public static double calculateTaxUSD(double unitPrice, int quantity, double taxRateDecimal) throws IllegalArgumentException
+	{
+		if (unitPrice < 0)
+		{
+			throw new IllegalArgumentException("Illegal unitPrice supplied. unitPrice should be >= 0.");
+		}
+		
+		if (taxRateDecimal < 0 || taxRateDecimal > 1)
+		{
+			throw new IllegalArgumentException("Illegal taxRateDecimal supplied. Tax rate should be between 0 and 1 (inclusive).");
+		}
+		
+		if (quantity < 0)
+		{
+			throw new IllegalArgumentException("Illegal quantity supplied. Quantity should be >= 0.");
+		}
+		
+		return roundToTwoDecimalPlaces(unitPrice * quantity * taxRateDecimal);
+	}
+	
+	/**
+	 * Rounds the supplied value to two decimal places.
+	 * @param valueToRound - The value that needs to be rounded.
+	 * @return - Returns the valueToRound rounded to two decimal places, as a double.
+	 */
+	private static double roundToTwoDecimalPlaces(double valueToRound)
+	{
+		return BigDecimal.valueOf(valueToRound).setScale(2, BigDecimal.ROUND_HALF_DOWN).doubleValue(); 
+	}
+}
---
+++ OrderCalculation/src/test/java/com/tek/interview/question/ordercalculation/impl/TaxCalculatorUtilsTest.java
@@ -0,0 +1,138 @@
+/**
+ * 
+ */
+package com.tek.interview.question.ordercalculation.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Steve Whitmire (swhit114@gmail.com)
+ *
+ */
+public class TaxCalculatorUtilsTest
+{
+	/**
+	 * Test method for {@link com.tek.interview.question.ordercalculation.impl.TaxCalculatorUtils#calculateTaxUSD(double, double)}.
+	 */
+	@Test(expected=IllegalArgumentException.class)
+	public void testCalculateTaxUSDDoubleDouble_InvalidUnitPrice()
+	{
+		TaxCalculatorUtils.calculateTaxUSD(-0000000000000000000000000000000.0000000000000000000000000001, .12349);
+	}
+	
+	/**
+	 * Test method for {@link com.tek.interview.question.ordercalculation.impl.TaxCalculatorUtils#calculateTaxUSD(double, double)}.
+	 */
+	@Test(expected=IllegalArgumentException.class)
+	public void testCalculateTaxUSDDoubleDouble_InvalidTaxRateDecimal1()
+	{
+		TaxCalculatorUtils.calculateTaxUSD(12.443, -00000000000000000000000000000.00000000000000000001);
+	}
+	
+	/**
+	 * Test method for {@link com.tek.interview.question.ordercalculation.impl.TaxCalculatorUtils#calculateTaxUSD(double, double)}.
+	 */
+	@Test(expected=IllegalArgumentException.class)
+	public void testCalculateTaxUSDDoubleDouble_InvalidTaxRateDecimal2()
+	{
+		TaxCalculatorUtils.calculateTaxUSD(12.443, 1.00000000001);
+	}
+	
+	/**
+	 * Test method for {@link com.tek.interview.question.ordercalculation.impl.TaxCalculatorUtils#calculateTaxUSD(double, double)}.
+	 */
+	@Test
+	public void testCalculateTaxUSDDoubleDouble_ValidCalculations()
+	{
+		TaxCalculatorUtils.calculateTaxUSD(0000.0001, 0000.000001);
+		TaxCalculatorUtils.calculateTaxUSD(00000.00000001, .99999999);
+		TaxCalculatorUtils.calculateTaxUSD(0, .99999999);
+		TaxCalculatorUtils.calculateTaxUSD(123, .45);
+		TaxCalculatorUtils.calculateTaxUSD(123.001, .4500);
+	}
+	
+	/**
+	 * Test method for {@link com.tek.interview.question.ordercalculation.impl.TaxCalculatorUtils#calculateTaxUSD(double, double)}.
+	 */
+	@Test
+	public void testCalculateTaxUSDDoubleDouble_ValidReturns()
+	{
+		assertTrue(TaxCalculatorUtils.calculateTaxUSD(12.49, .1) == 1.25);
+		assertTrue(TaxCalculatorUtils.calculateTaxUSD(14.99, .1) == 1.5);
+		assertTrue(TaxCalculatorUtils.calculateTaxUSD(47.50, .15) == 7.12);
+		assertTrue(TaxCalculatorUtils.calculateTaxUSD(27.99, .15) == 4.2);
+		
+		assertTrue(TaxCalculatorUtils.calculateTaxUSD(42.52, .1) == 4.25);
+		assertTrue(TaxCalculatorUtils.calculateTaxUSD(48.57, .15) == 7.29);
+	}
+
+	/**
+	 * Test method for {@link com.tek.interview.question.ordercalculation.impl.TaxCalculatorUtils#calculateTaxUSD(double, int, double)}.
+	 */
+	@Test(expected=IllegalArgumentException.class)
+	public void testCalculateTaxUSDDoubleIntDouble_InvalidUnitPrice()
+	{
+		TaxCalculatorUtils.calculateTaxUSD(-00000000.00001, 2, .1234);
+	}
+	
+	/**
+	 * Test method for {@link com.tek.interview.question.ordercalculation.impl.TaxCalculatorUtils#calculateTaxUSD(double, int, double)}.
+	 */
+	@Test(expected=IllegalArgumentException.class)
+	public void testCalculateTaxUSDDoubleIntDouble_InvalidQuantity()
+	{
+		TaxCalculatorUtils.calculateTaxUSD(132.34, -1, .34325);
+	}
+	
+	/**
+	 * Test method for {@link com.tek.interview.question.ordercalculation.impl.TaxCalculatorUtils#calculateTaxUSD(double, int, double)}.
+	 */
+	@Test(expected=IllegalArgumentException.class)
+	public void testCalculateTaxUSDDoubleIntDouble_InvalidTaxRateDecimal1()
+	{
+		TaxCalculatorUtils.calculateTaxUSD(144, 12, -00.000000001);
+	}
+	
+	/**
+	 * Test method for {@link com.tek.interview.question.ordercalculation.impl.TaxCalculatorUtils#calculateTaxUSD(double, int, double)}.
+	 */
+	@Test(expected=IllegalArgumentException.class)
+	public void testCalculateTaxUSDDoubleIntDouble_InvalidTaxRateDecimal2()
+	{
+		TaxCalculatorUtils.calculateTaxUSD(144, 12, 1.00123);
+	}
+	
+	/**
+	 * Test method for {@link com.tek.interview.question.ordercalculation.impl.TaxCalculatorUtils#calculateTaxUSD(double, int, double)}.
+	 */
+	@Test
+	public void testCalculateTaxUSDDoubleIntDouble_ValidCalculations()
+	{
+		TaxCalculatorUtils.calculateTaxUSD(0, 12, 0);
+		TaxCalculatorUtils.calculateTaxUSD(0.1, 12, 0);
+		TaxCalculatorUtils.calculateTaxUSD(1, 12, 0);
+		TaxCalculatorUtils.calculateTaxUSD(.99, 12, 0);
+		TaxCalculatorUtils.calculateTaxUSD(12.34, 1, .54);
+		TaxCalculatorUtils.calculateTaxUSD(12.34, 99, .523);
+		TaxCalculatorUtils.calculateTaxUSD(4435.5d, 2, 1);
+	}	
+	
+	/**
+	 * Test method for {@link com.tek.interview.question.ordercalculation.impl.TaxCalculatorUtils#calculateTaxUSD(double, int, double)}.
+	 */
+	@Test
+	public void testCalculateTaxUSDDoubleIntDouble_ValidReturns()
+	{
+		assertTrue(TaxCalculatorUtils.calculateTaxUSD(12.49,  1, .1) == 1.25);
+		assertTrue(TaxCalculatorUtils.calculateTaxUSD(14.99,  1, .1) == 1.5);
+		assertTrue(TaxCalculatorUtils.calculateTaxUSD(47.50,  1, .15) == 7.12);
+		assertTrue(TaxCalculatorUtils.calculateTaxUSD(27.99,  1, .15) == 4.2);
+		
+		assertTrue(TaxCalculatorUtils.calculateTaxUSD(42.52, 2, .1) == 8.5);
+		assertTrue(TaxCalculatorUtils.calculateTaxUSD(48.57, 2, .15) == 14.57);
+	}
+}
